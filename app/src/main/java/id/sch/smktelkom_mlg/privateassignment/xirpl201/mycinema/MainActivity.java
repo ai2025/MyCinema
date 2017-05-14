9468d320f0650f1a073c4fd7e3bb53059776d281
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2017-05-14 22:33:05 +0700

    Last Commit; Aisyatush Shofiah

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl201/mycinema/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl201/mycinema/MainActivity.java
@@ -1,8 +1,7 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl201.mycinema;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,7 +16,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import id.sch.smktelkom_mlg.privateassignment.xirpl201.mycinema.adapter.SourceAdapter;
+
+public class MainActivity extends AppCompatActivity implements SourceAdapter.ISourceAdapter {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -52,15 +53,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
@@ -79,11 +71,18 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.ic_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void showArticles(String id, String name, String sortBy) {
+
     }
 
     /**
@@ -135,24 +134,27 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            if (position == 0)
+                return new NowPlayingFragment();
+            else if (position == 1)
+                return new ComingSoonFragment();
+            else
+                return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Now Playing";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "Coming Soon";
             }
             return null;
         }
